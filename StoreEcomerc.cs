c54d8d839880f07b7ddf815488aaf9585b0f9850
Vadim Klimenko <vadim_klimenko@mail.ru>
2021-03-31 12:33:35 +0300

    Add project files.

---
+++ StoreEcomerc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+namespace StoreTesting
+{
+    [TestFixture]
+    public class StoreEcomerc
+    {
+        private IWebDriver driver;
+        
+        private string url = "http://litecart/";
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void LoginAdminPanel()
+        {
+            driver.Url = $"{url}admin/";
+
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+
+    }       
+}
